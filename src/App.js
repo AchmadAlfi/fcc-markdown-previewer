fdebde033b88c7325f160dee38a39afad237e19b
AchmadAlfi <achmadalfi199@gmail.com>
2020-08-26 23:11:43 +0700

    Add markdown state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Form from "react-bootstrap/Form";
 let marked = require("marked");
 
 class App extends Component {
+  state = {
+    markdown: "",
+  };
+
   render() {
+    let { markdown } = this.state;
     return (
       <div className="App px-2">
         <div>
@@ -17,9 +22,14 @@ class App extends Component {
                 as="textarea"
                 placeholder="Enter Markdown"
                 rows="3"
+                value={markdown}
               ></Form.Control>
             </Form.Group>
           </Form>
+        </div>
+        <div>
+          <h1>Markdown Output</h1>
+          <div></div>
         </div>
       </div>
     );
